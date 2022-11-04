ef84efcaa4b4713bf7c92964903e78a6d8365ad8
kimsongjun <kinsongjun13@gmail.com>
2022-11-04 16:12:56 +0900

    [Edit] 1104 store.js에 전역으로 tierUnit 저장함(실시간 필터링 되는 값)

--- Vuelolchess/src/store.js
+++ Vuelolchess/src/store.js
@@ -1,16 +1,19 @@
-import { createStore } from "vuex";
-import axios from "axios";
+import { createStore } from 'vuex';
+import axios from 'axios';
+// import TierUnit from './assets/tierUnit.json';
+// import Newdata from './assets/newdata.json';
 
 const store = createStore({
   state() {
     return {
       page: 0,
-      name: "",
+      name: '',
       matchData: [],
       matchData2: [],
       matchData3: [],
       matchData4: [],
       matchData5: [],
+      tierUnit: {},
     };
   },
   mutations: {
@@ -35,6 +38,9 @@ const store = createStore({
     SetMatchData5(state, inputValue) {
       state.SetMatchData5 = inputValue;
     },
+    SetTierUnit(state, inputValue) {
+      state.tierUnit = inputValue;
+    },
   },
   // ajax 요청 받는거
   actions: {
@@ -47,7 +53,7 @@ const store = createStore({
           {
             transformRequest: [
               (data, headers) => {
-                delete headers.common["X-Requested-With"];
+                delete headers.common['X-Requested-With'];
                 return data;
               },
             ],
@@ -58,10 +64,10 @@ const store = createStore({
           console.log(`GetMatchHistory8659: ${result.data}`);
           console.log(result.data);
           console.log(result);
-          context.commit("SetMatchData", result.data);
+          context.commit('SetMatchData', result.data);
         })
         .catch((e) => {
-          console.log("error-GetMatchhistory8659");
+          console.log('error-GetMatchhistory8659');
           console.log(e);
         });
     },
@@ -74,7 +80,7 @@ const store = createStore({
           {
             transformRequest: [
               (data, headers) => {
-                delete headers.common["X-Requested-With"];
+                delete headers.common['X-Requested-With'];
                 return data;
               },
             ],
@@ -85,10 +91,10 @@ const store = createStore({
           console.log(`GetMatchHistory8660: ${result.data}`);
           console.log(result.data);
           console.log(result);
-          context.commit("SetMatchData2", result.data);
+          context.commit('SetMatchData2', result.data);
         })
         .catch((e) => {
-          console.log("error-GetMatchhistory8660");
+          console.log('error-GetMatchhistory8660');
           console.log(e);
         });
     },
@@ -101,7 +107,7 @@ const store = createStore({
           {
             transformRequest: [
               (data, headers) => {
-                delete headers.common["X-Requested-With"];
+                delete headers.common['X-Requested-With'];
                 return data;
               },
             ],
@@ -112,10 +118,10 @@ const store = createStore({
           console.log(`GetMatchHistory8080: ${result.data}`);
           console.log(result.data);
           console.log(result);
-          context.commit("SetMatchData3", result.data);
+          context.commit('SetMatchData3', result.data);
         })
         .catch((e) => {
-          console.log("error-GetMatchhistory8080");
+          console.log('error-GetMatchhistory8080');
           console.log(e);
         });
     },
@@ -128,7 +134,7 @@ const store = createStore({
           {
             transformRequest: [
               (data, headers) => {
-                delete headers.common["X-Requested-With"];
+                delete headers.common['X-Requested-With'];
                 return data;
               },
             ],
@@ -139,10 +145,10 @@ const store = createStore({
           console.log(`GetMatchHistory: ${result.data}`);
           console.log(result.data);
           console.log(result);
-          context.commit("SetMatchData4", result.data);
+          context.commit('SetMatchData4', result.data);
         })
         .catch((e) => {
-          console.log("error-GetMatchhistory");
+          console.log('error-GetMatchhistory');
           console.log(e);
         });
     },
@@ -155,7 +161,7 @@ const store = createStore({
           {
             transformRequest: [
               (data, headers) => {
-                delete headers.common["X-Requested-With"];
+                delete headers.common['X-Requested-With'];
                 return data;
               },
             ],
@@ -166,10 +172,10 @@ const store = createStore({
           console.log(`GetMatchHistory: ${result.data}`);
           console.log(result.data);
           console.log(result);
-          context.commit("SetMatchData5", result.data);
+          context.commit('SetMatchData5', result.data);
         })
         .catch((e) => {
-          console.log("error-GetMatchhistory");
+          console.log('error-GetMatchhistory');
           console.log(e);
         });
     },
