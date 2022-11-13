2b4952e8966d166913074a40e60b489fc3afca2d
kimsongjun <kinsongjun13@gmail.com>
2022-11-13 15:33:57 +0900

    [Edit] 1113 아이템 style 적용 안된부분 수정

--- Vuelolchess/src/components/Item/ItemPage.vue
+++ Vuelolchess/src/components/Item/ItemPage.vue
@@ -24,12 +24,12 @@
 </template>
 
 <script>
-import Header from "../Header.vue";
-import Filter from "../Filter/ItemFilterContainer.vue";
-import ItemTable from "./ItemTable.vue";
-import ItemApex from "./ItemApex.vue";
-import ItemInfo from "./ItemInfo.vue";
-import Footer from "../Footer.vue";
+import Header from '../Header.vue';
+import Filter from '../Filter/ItemFilterContainer.vue';
+import ItemTable from './ItemTable.vue';
+import ItemApex from './ItemApex.vue';
+import ItemInfo from './ItemInfo.vue';
+import Footer from '../Footer.vue';
 
 export default {
   components: {
@@ -53,7 +53,7 @@ export default {
       this.itemID = id;
     },
     reset() {
-      console.log("reset");
+      console.log('reset');
     },
     changeContent(content) {
       this.container = content;
@@ -68,9 +68,9 @@ export default {
   grid-template-columns: 1fr 5fr 1fr;
   justify-items: stretch;
   grid-template-areas:
-    "header header header"
-    "a filter b"
-    "footer footer footer";
+    'header header header'
+    'a filter b'
+    'footer footer footer';
   height: 100vh;
   align-items: stretch;
   background-color: rgb(10, 10, 26);
@@ -93,6 +93,7 @@ export default {
   height: 94%;
 }
 .item-content {
+  background-color: rgb(10, 10, 26);
   padding: 0px 0px 20px;
 }
 </style>
